e7ed190328cc5d61f640bb841b24ea6ed1f0c7c5
SpikeScarlatti <pilote.helicoptere@gmail.com>
2020-05-21 16:48:24 +0200

    LuaDocCreator V1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+    private static final String OUTPUT_FILENAME = "luaDocOutput_SpikeScarlatti.lua";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Project path or lua file path : ");
@@ -11,21 +13,21 @@ public class Main {
         LuaProject luaProject = new LuaProject(new File(path));
         if(!luaProject.isContainsLuaFile()){
             System.out.println("Project does not have .lua file !");
-        }
+        }else{
+            String output = luaProject.getOutput();
 
-        System.out.println("Save output ? (Y/N)");
+            System.out.println("Save output ? (Y/N)");
+            String saveOutput = sc.next();
 
-        String saveOutput = sc.next();
-        String output = luaProject.getOutput();
-        String header = "--- Output generated from LuaDocCreator created by Spike Scarlatti. https://github.com/SpikeScarlatti/LuaDocCreator";
-        System.out.println(header);
-        System.out.println();
-        System.out.println(output);
+            String header = "--- Output generated from LuaDocCreator created by Spike Scarlatti. https://github.com/SpikeScarlatti/LuaDocCreator";
+            System.out.println(header);
+            System.out.println();
+            System.out.println(output);
 
-        if(saveOutput.toUpperCase().equals("Y")){
-            String filename = "luaDocOutput_SpikeScarlatti.lua";
-            TxtFile outputFile = new TxtFile(filename, path +"\\" + filename);
-            outputFile.saveFile(header, output);
+            if(saveOutput.toUpperCase().equals("Y")){
+                TxtFile outputFile = new TxtFile(OUTPUT_FILENAME, path +"\\" + OUTPUT_FILENAME);
+                outputFile.saveFile(header, output);
+            }
         }
     }
 }
